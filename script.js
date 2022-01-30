ca184aaf6052c75ae96f9bb31acffa0cbfe9b077
Yaroslav777999 <88874725+Yaroslav777999@users.noreply.github.com>
2022-01-30 22:10:04 +0400

    исправил вложенность

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ window.addEventListener('DOMContentLoaded', function () {
   var1Changed = document.querySelector('#var1');
 
   createTextBtn.addEventListener("click", function () {
-    textEl.insertAdjacentHTML('afterbegin', 
-    `<div>
+    textEl.insertAdjacentHTML('afterbegin',
+      `<div>
     Жили-были <span id="s1">{var1}</span> да {var2},
     Была у них {var3},
     Снесла {var3} {var4}, не простое - золотое,
@@ -15,14 +15,12 @@ window.addEventListener('DOMContentLoaded', function () {
     {var5} бежала, {var6} задела, {var4} упало и разбилось.,
     {var1} плачет, {var2} плачет, а {var3} кудахчет:,{speach}
     </div>`);
-})
-var1InputEl = document.querySelector('#var1');
-span1InputEl = document.querySelector('#s1');
-
-changeTextBtn.addEventListener("click", function () {
-  span1InputEl.addEventListener("click", function () {
-    span1InputEl.textContent = var1InputEl.target.value;
-  })      
   })
-})
+  var1InputEl = document.querySelector('#var1');
+  span1InputEl = document.querySelector('#s1');
 
+  changeTextBtn.addEventListener("click", function () {
+      span1InputEl.textContent = var1InputEl.value;
+  })
+
+})
