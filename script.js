053bca2fe52624df096f0b41a412dea529371a0e
Yaroslav777999 <88874725+Yaroslav777999@users.noreply.github.com>
2022-02-02 13:58:13 +0400

    рабочий проект

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', function () {
   let changeTextBtn = document.querySelector('.btn-change');
   let createTextBtn = document.querySelector('.btn-create');
   let textEl = document.querySelector('.text');
-  // let var1Changed = document.querySelector('#var1');
 
   createTextBtn.addEventListener("click", function () {
     textEl.insertAdjacentHTML('afterbegin',
@@ -20,21 +19,6 @@ window.addEventListener('DOMContentLoaded', function () {
   })
 
   changeTextBtn.addEventListener("click", function () {
-    // var1InputEl = document.querySelector('.js-input-1');
-    // var2InputEl = document.querySelector('.js-input-2');
-    // var3InputEl = document.querySelector('.js-input-3');
-    // var4InputEl = document.querySelector('.js-input-4');
-    // var5InputEl = document.querySelector('.js-input-5');
-    // var6InputEl = document.querySelector('.js-input-6');
-    // varSpeechInputEl = document.querySelector('.js-input-7');
-
-    // span1InputEl = document.querySelector('.js-var-1');
-    // span2InputEl = document.querySelector('.js-var-2');
-    // span3InputEl = document.querySelector('.js-var-3');
-    // span4InputEl = document.querySelector('.js-var-4');
-    // span5InputEl = document.querySelector('.js-var-5');
-    // span6InputEl = document.querySelector('.js-var-6');
-    // spanSpeechInputEl = document.querySelector('.js-var-7');
 
     function check(param) {
       return param.value.length > 0
@@ -45,24 +29,16 @@ window.addEventListener('DOMContentLoaded', function () {
       return createTextBtn.classList.contains('hidden');
     }
 
-    changeTextBtn.addEventListener("click", function () {
-      const arr_test = [1, 2, 3, 4, 5, 6, 7]
+    const arr_test = [1, 2, 3, 4, 5, 6, 7]
 
-      arr_test.forEach((element) => {
-        let varInput = document.querySelector(`.js-input-${element}`);
-        if (checkBtnPush() && check(varInput)) {
-          let varTest = document.querySelector(`.js-var-${element}`);
-          varTest.textContent = varInput.value;
-        }
-      });
-
-      // var1InputEl.value.length !== 0 ? span1InputEl.textContent = var1InputEl.value : "";
-      // var2InputEl.value.length !== 0 ? span2InputEl.textContent = var2InputEl.value : "";
-      // var3InputEl.value.length !== 0 ? span3InputEl.textContent = var3InputEl.value : "";
-      // var4InputEl.value.length !== 0 ? span4InputEl.textContent = var4InputEl.value : "";
-      // var5InputEl.value.length !== 0 ? span5InputEl.textContent = var5InputEl.value : "";
-      // var6InputEl.value.length !== 0 ? span6InputEl.textContent = var6InputEl.value : "";
-      // varSpeechInputEl.value.length !== 0 ? spanSpeechInputEl.textContent = varSpeechInputEl.value : "";
-    })
+    arr_test.forEach((element) => {
+      let varInput = document.querySelector(`.js-input-${element}`);
+      if (checkBtnPush() && check(varInput)) {
+        let varTest = document.querySelectorAll(`.js-var-${element}`);
+        varTest.forEach((el) => {
+          el.textContent = varInput.value
+        })
+      }
+    });
   })
 })
